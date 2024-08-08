a8f4e7cd302d730fa54deaaee161125054a4860f
BlackNereus <116273585+BlackNereus@users.noreply.github.com>
2024-08-08 10:31:44 +0000

    generador de excusas- Maikel Contramaestre

--- src/app.js
+++ src/app.js
@@ -8,4 +8,41 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  console.log("El DOM ha cargado correctamente");
+  document.querySelector("#butt").addEventListener("click", function() {
+    document.querySelector("#excusa").innerHTML = geneexcusa();
+  });
+  function geneexcusa() {
+    let sujeto = [
+      "Jose",
+      "Mi mamá",
+      "Mi gato",
+      "El tráfico",
+      "El internet",
+      "El perro"
+    ];
+    let accion = [
+      "se comió",
+      "perdió",
+      "desapareció",
+      "llegó",
+      "tocó",
+      "limpió",
+      "encontró"
+    ];
+    let donde = [
+      "el carro",
+      "mi tarea",
+      "las llaves",
+      "el portátil",
+      "la carne"
+    ];
+
+    let sujetoin = sujeto[Math.floor(Math.random() * sujeto.length)];
+    let accionin = accion[Math.floor(Math.random() * accion.length)];
+    let dondein = donde[Math.floor(Math.random() * donde.length)];
+
+    return sujetoin + " " + accionin + " " + dondein + ".";
+  }
 };
